52c68dd51fb723e7ebf8554f5b3d0af869274da7
Juan DAvid <juanda.o.ochoa@gmail.com>
2025-03-18 20:32:54 -0500

    Actualización de codigo

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let myfriends = [];
+
+function añadirAmigo() {
+    const inputName = document.getElementById("amigo");
+    const name = inputName.value.trim();
+    
+    if (name === "") { 
+    alert("Por favor, inserte un nombre.");
+    } else{
+        myfriends.push(name);
+        inputName.value = "";
+        actualizarListaAmigos();
+    }
+}
+function actualizarListaAmigos() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; 
+    for (let friend of myfriends) {
+        const li = document.createElement("li");
+        li.textContent = friend;
+        lista.appendChild(li);
+    }
+}
+
+
+function sortearAmigo() {
+    if (myfriends.length === 0) {
+        alert("No hay amigos para sortear. ¡Agrega algunos amigos primero!");
+        return;
+    }
+
+    
+    const indiceAleatorio = Math.floor(Math.random() * myfriends.length);
+    const amigoSorteado = myfriends[indiceAleatorio];
+
+   
+    const resultado = document.getElementById("resultado");
+    resultado.textContent = `El amigo secreto es: ${amigoSorteado}`;
+}
+
+
+document.querySelector("btnAñadir").addEventListener("click", añadirAmigo);
+document.getElementById("btnSortear").addEventListener("click", sortearAmigo);
